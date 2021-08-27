ecfa534b4f5dda5d40be1888da48d523e4300bbf
Aditya <wasnikaditya56@gmail.com>
2021-08-27 21:38:07 +0530

    Roomdatabase,ViewModel

---
+++ app/src/main/java/com/aditya/roomdbapps/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.aditya.roomdbapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
+import com.aditya.roomdbapps.db.UserEntity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), RecyclerViewAdapter.RowClickListener {
+
+    lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    lateinit var viewModel: MainActivityViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            recyclerViewAdapter = RecyclerViewAdapter(this@MainActivity)
+            adapter = recyclerViewAdapter
+            val divider = DividerItemDecoration(applicationContext, VERTICAL)
+            addItemDecoration(divider)
+        }
+
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        viewModel.getAllUsersObservers().observe(this, Observer {
+            recyclerViewAdapter.setListData(ArrayList(it))
+            recyclerViewAdapter.notifyDataSetChanged()
+        })
+
+
+        saveButton.setOnClickListener {
+            val name  = nameInput.text.toString()
+            val email  = emailInput.text.toString()
+            val phone = phoneInput.text.toString()
+            if(saveButton.text.equals("Save")) {
+                val user = UserEntity(0, name, email, phone)
+                viewModel.insertUserInfo(user)
+            } else {
+                val user = UserEntity(nameInput.getTag(nameInput.id).toString().toInt(), name, email, phone)
+                viewModel.updateUserInfo(user)
+                saveButton.setText("Save")
+            }
+            nameInput.setText("")
+            emailInput.setText("")
+        }
+    }
+
+
+
+    override fun onDeleteUserClickListener(user: UserEntity) {
+        viewModel.deleteUserInfo(user)
+    }
+
+    override fun onItemClickListener(user: UserEntity) {
+        nameInput.setText(user.name)
+        emailInput.setText(user.email)
+        phoneInput.setText(user.phone)
+        nameInput.setTag(nameInput.id, user.id)
+        saveButton.setText("Update")
+    }
+}
